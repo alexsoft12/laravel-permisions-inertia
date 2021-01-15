76ddeb4cb91b5fc74402f2a2e6757bbc06e2724f
Yuler Figueroa <yuleralex1@gmail.com>
2021-01-14 21:42:57 -0500

    initial commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+        <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            <inertia-link v-if="$page.props.user" href="/dashboard" class="text-sm text-gray-700 underline">
+                Dashboard
+            </inertia-link>
+
+            <template v-else>
+                <inertia-link :href="route('login')" class="text-sm text-gray-700 underline">
+                    Login
+                </inertia-link>
+
+                <inertia-link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 underline">
+                    Register
+                </inertia-link>
+            </template>
+        </div>
+
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+                <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                    Sistema de usuarios
+                </h1>
+            </div>
+
+            <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                <div class="grid grid-cols-1 md:grid-cols-2">
+                    <div class="p-6">
+                        <div class="flex items-center">
+                            <div class="ml-4 text-lg leading-7 font-semibold"><a href="#" class="underline text-gray-900 dark:text-white">Ventas</a></div>
+                        </div>
+
+                        <div class="ml-12">
+                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                Modulo de Ventas
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
+                        <div class="flex items-center">
+
+                            <div class="ml-4 text-lg leading-7 font-semibold"><a href="#" class="underline text-gray-900 dark:text-white">Compras</a></div>
+                        </div>
+
+                        <div class="ml-12">
+                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                               Modulo de Compras
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="p-6 border-t border-gray-200 dark:border-gray-700">
+                        <div class="flex items-center">
+
+                            <div class="ml-4 text-lg leading-7 font-semibold"><a href="#" class="underline text-gray-900 dark:text-white">Gestión de usuarios</a></div>
+                        </div>
+
+                        <div class="ml-12">
+                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                Modulo gestión Usuarios
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
+                        <div class="flex items-center">
+
+                            <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Reportes</div>
+                        </div>
+
+                        <div class="ml-12">
+                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                Modulo de Reportes
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
+
+                <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                   Autor: AlexSoft | Versión: v0.1
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        props: {
+            canLogin: Boolean,
+            canRegister: Boolean,
+            laravelVersion: String,
+            phpVersion: String,
+        }
+    }
+</script>
